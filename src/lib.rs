07f02d63591a7cb01ef634595eeeed78edd9105e
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2025-06-20 01:55:57 +0300

    initialiaze the function and str byte variable

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
 // Implement extract_tx_version function below
 pub fn extract_tx_version(raw_tx_hex: &str) -> Result<u32, String> {
-    todo!()
+    // Try to decode the hex string into bytes
+    let str_bytes = hex::decode(raw_tx_hex).map_err(|e| format!("Hex decode error: {}", e))?;
+
+
+    Ok(version)
 }
